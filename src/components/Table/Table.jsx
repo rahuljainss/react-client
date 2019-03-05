78ae929f79ece4fba19b49f505d85fd1ec14ee85
rahuljainss <rahul.jain@successive.tech>
2019-03-05 16:13:45 +0530

    docs: Adding sort event on table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+// import { format } from 'path';
 
 const styles = theme => ({
   root: {
@@ -23,39 +26,74 @@ const styles = theme => ({
   base: {
     fontSize: '14px',
   },
+  row: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
 });
+class SimpleTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
-function SimpleTable(props) {
-  const { classes, column, data } = props;
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead className={classes.base1}>
-          <TableRow>
-            {
-              column.map(header => (
-                <TableCell align={header.align}>{header.label}</TableCell>
-              ))
-            }
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map(trainee => (
-            <TableRow>
-              <TableCell align="center" className={classes.base}>{trainee.name}</TableCell>
-              <TableCell className={classes.base}>{trainee.email}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+  render() {
+    const {
+      classes, column, data, orderBy, order, onSort, onSelect,
+    } = this.props;
+    return (
+      <>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead className={classes.base1}>
+              <TableRow>
+                {column.map(header => (
+                  // <TableCell align={header.align}>{header.label}</TableCell>
+                  <TableCell align={header.align}>
+                    <TableSortLabel
+                      active={orderBy === header.field}
+                      direction={order}
+                      onClick={event => onSort(event, header.field)}
+                    >
+                      {header.label}
+                    </TableSortLabel>
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody stripedRows>
+              {data.map(trainee => (
+                <TableRow
+                  hover
+                  className={classes.row}
+                  key={trainee.id}
+                  onClick={event => onSelect(event, trainee.id)}
+                >
+                  {column.map(header => (
+                    <TableCell
+                      className={classes.base}
+                      align={header.align}
+                    >
+                      {header.format
+                        ? header.format(trainee[header.field])
+                        : trainee[header.field]}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      </>
+    );
+  }
 }
-
 SimpleTable.propTypes = {
-  classes: PropTypes.objectOf(PropTypes.string).isRequired,
-  column: PropTypes.objectOf(PropTypes.string).isRequired,
+  classes: PropTypes.objectOf(PropTypes.object).isRequired,
+  column: PropTypes.objectOf(PropTypes.object).isRequired,
   data: PropTypes.objectOf(PropTypes.string).isRequired,
 };
-
+SimpleTable.propTypes = PropTypes;
 export default withStyles(styles)(SimpleTable);
